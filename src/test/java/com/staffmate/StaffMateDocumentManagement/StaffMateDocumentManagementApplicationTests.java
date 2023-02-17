bee85b30c2774eb946bc7fc9bdfc8ccd88e606a7
Lea Kalmar <lea.kalmar@gmail.com>
2023-02-17 07:46:52 +0100

    initialize project

---
+++ src/test/java/com/staffmate/StaffMateDocumentManagement/StaffMateDocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffmate.StaffMateDocumentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffMateDocumentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
